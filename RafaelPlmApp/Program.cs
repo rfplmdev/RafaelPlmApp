6951b8927a08abdfc24f0ded76af22075c0b5868
OHADH <OHADH@D-C-OHADH-NW>
2022-05-31 14:48:47 +0300

    create new project

--- RafaelPlmApp/Program.cs
+++ RafaelPlmApp/Program.cs
@@ -13,6 +13,9 @@ namespace RafaelPlmApp
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
+
+            List<int> numlist = new List<int> { 55, 66, 7, 90, 454, 123, 656, 865, 45647, 6, 65 };
+            Parallel.ForEach(numlist, (num) => { Console.WriteLine(num); });
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
